37eadd0b718188e2b061ba225f515ca1734aee86
Alexey <kalnitskiy2018@mail.ru>
2023-07-30 14:16:09 +0500

    ex2

---
+++ ex2/Program.cs
@@ -0,0 +1,21 @@
+﻿int[,] arr = new int[4,5];
+
+for(int i = 0; i < arr.GetLength(0); i++){
+    for(int j = 0; j < arr.GetLength(1); j++){
+        arr[i,j] = new Random().Next(10,50);
+        System.Console.Write("["+arr[i,j]+"]"+" ");
+    }
+    System.Console.WriteLine();
+}
+
+int indexI = Convert.ToInt32(Console.ReadLine());
+int indexJ = Convert.ToInt32(Console.ReadLine());
+for(int i = 0; i < arr.GetLength(0); i++){
+    for(int j = 0; j < arr.GetLength(1); j++){
+        if(arr[indexI,indexJ] == arr[i,j]){
+            System.Console.Write("["+arr[indexI,indexJ]+"]"+" ");
+        }
+       
+    }
+}
+
