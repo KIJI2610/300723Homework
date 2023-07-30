5d40c1d91ada70102119db78482e338f7222ab76
Alexey <kalnitskiy2018@mail.ru>
2023-07-30 13:53:08 +0500

    ex1

---
+++ ex1/Program.cs
@@ -0,0 +1,13 @@
+﻿System.Console.Write("Введите длину: ");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите высоту: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+double[,] arr = new double[b,a];
+for(int i = 0; i < arr.GetLength(0); i++){
+    for(int j = 0; j < arr.GetLength(1); j++){
+    arr[i,j] = new Random().NextDouble()*(-100+50)+35;
+    System.Console.Write("["+arr[i,j].ToString("F2")+"]"+" ");
+}
+System.Console.WriteLine();
+}
